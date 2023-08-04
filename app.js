0ab494b98eb2206b7f0a0f96e3944a105f2b7bd0
ilu25 <acejsy@gmail.com>
2023-08-04 11:39:27 +0900

    [style] : 불필요한 코드 삭제

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/popups', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
