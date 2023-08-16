e14e855d139468d90a5ef2bcecfdfc8be14cf43e
ilu25 <acejsy@gmail.com>
2023-08-16 17:31:36 +0900

    [feat] : 유저 validation 추가 및 변경

--- models/User.js
+++ models/User.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     user_id: {
         type: String,
-        required: true,
+        required: '아이디를 입력하세요.',
         unique: true,
+        match: [/^[A-Za-z]{1}[A-Za-z0-9]{3,14}$/, '영문으로 시작해서 영문/숫자를 포함하여 4 ~ 15자로 입력하세요.']
     },
     password: {
         type: String,
-        required: true,
+        required: '비밀번호를 입력하세요.',
     },
     is_deleted: {
         type: Boolean,
