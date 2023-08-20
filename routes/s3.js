a26739ba32c76fa3b490618d2e1ecbc36731ec22
ulsandonghun <donghoon141@naver.com>
2023-08-20 23:09:06 +0900

    [feat] : S3 이미지 업로드 기능 구현(jpg로 형식변환전)

--- routes/s3.js
+++ routes/s3.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const imageUploader = require("../utils/ImageUploader");
 
+
+
 router.post('/test/image', imageUploader.single('image'), (req, res) => {
-    res.send('Image uploaded successfully!');
+    // 이미지 업로드 후 URL에 접근하여 클라이언트로 응답
+    const imageUrl = req.file.location;
+    res.json({ imageUrl: imageUrl });
 });
 
 module.exports = router;
-
-
-router.post('/test/image',imageUploader.single('image'),(req,res) =>{
-
-    res.send('사진파일 1개 전송완료');
-})
-
-module.exports =router;
