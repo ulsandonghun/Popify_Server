3b944cc58af58dd00a11cf5215c2462a6ba448e5
ulsandonghun <donghoon141@naver.com>
2023-08-21 10:35:30 +0900

    [feat] s3 이미지 업로드후 url 반환 기능 구현

--- routes/s3.js
+++ routes/s3.js
@@ -4,7 +4,7 @@ const imageUploader = require("../utils/ImageUploader");
 
 
 
-router.post('/test/image', imageUploader.single('image'), (req, res) => {
+router.post('/upload/image', imageUploader.single('image'), (req, res) => {
     // 이미지 업로드 후 URL에 접근하여 클라이언트로 응답
     const imageUrl = req.file.location;
     res.json({ imageUrl: imageUrl });
--- utils/ImageUploader.js
+++ utils/ImageUploader.js
@@ -1,8 +1,7 @@
-const AWS =require( 'aws-sdk');
-const multer =require( 'multer');
+const AWS = require('aws-sdk');
+const multer = require('multer');
 const multerS3 = require('multer-s3');
-const path = require('path'); // path 모듈을 추가로 임포트합니다.
-
+const path = require('path');
 
 AWS.config.update({
     region:'ap-northeast-2',
@@ -11,25 +10,45 @@ AWS.config.update({
 
 });
 
-const s3 = new AWS.S3()
+const s3 = new AWS.S3();
 
-const allowedExtensions=['.png','.jpg','.jpeg','.bmp'];
+const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp'];
 
-const imageUploader =multer({
-
+const imageUploader = multer({
     storage: multerS3({
-
         s3: s3,
         bucket: 'nodeimage',
-        key: (req,file,callback) =>{
-            const uploadDirectory = req.query.directory ?? ''
-            const extension =path.extname(file.originalname)
-            if(!allowedExtensions.includes(extension)){
-                return callback(new Error('wrong extension'))
+        key: (req, file, callback) => {
+            const uploadDirectory = req.query.directory ?? '';
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new Error('wrong extension'));
             }
-            callback(null,`${uploadDirectory}/${Date.now()}_&{file.originalname}`)
+            callback(null, `${uploadDirectory}/${Date.now()}.${file.originalname.split('.').pop()}`);
         },
-        acl: 'public-read-write'
+        acl: 'public-read-write',
+        contentType: (req, file, callback) => {
+            // content type 불러오기.
+            const contentType = getContentType(file.originalname);
+            callback(null, contentType);
+        },
     }),
 });
+
+// 파일에 따라 메타 데이터 변경(이렇게 변경안하면 url 클릭시 다운로드 되버림.)
+function getContentType(filename) {
+    const extension = path.extname(filename);
+    switch (extension) {
+        case '.png':
+            return 'image/png';
+        case '.jpg':
+        case '.jpeg':
+            return 'image/jpeg';
+        case '.bmp':
+            return 'image/bmp';
+        default:
+            return 'application/octet-stream'; // 기본 content type
+    }
+}
+
 module.exports = imageUploader;
