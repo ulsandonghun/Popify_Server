46c2be2db0f0cdcbf364844ae4dd56c3a3e203f6
ilu25 <acejsy@gmail.com>
2023-08-26 22:36:56 +0900

    [remove] : 팝업스토어별 굿즈&리뷰 목록 조회 API 삭제

--- routes/goods.js
+++ routes/goods.js
@@ -14,23 +14,4 @@ router.get('/', async (req, res) => {
     }
 });
 
-/* 팝업스토어별 굿즈 목록 조회 */
-router.get('/popups/:popupId', async (req, res) => {
-    try {
-      const popupId = req.params.popupId;
-
-      // 팝업스토어 ID로 해당 팝업 정보 조회
-      const popup = await Popup.findById(popupId);
-  
-      if (!popup) {
-        return res.status(404).json({ message: '팝업스토어를 찾을 수 없습니다.' });
-      }
-      const goods = await Goods.find({ popup: popupId });
-
-      res.status(200).json(goods);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  });
-
 module.exports = router;
